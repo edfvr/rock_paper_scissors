c5694f66f9f7dbef9a1a12df3ece5c0ccc11bad3
Ed <edsagoefvr@gmail.com>
2022-10-22 19:25:48 +0000

    update player on moves

--- start.py
+++ start.py
@@ -1,1 +1,10 @@
-move = input("Rock, Paper, Scissors? ")
+import random
+
+
+player_move = input("Rock, Paper, Scissors? ")
+
+moves = ["rock", "paper", "scissors"]
+
+cpu_move = random.choice(moves)
+
+print(f"You chose {player_move}, cpu chose {cpu_move}.\n")
