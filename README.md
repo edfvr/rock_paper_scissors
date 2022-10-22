c4ff4acc5a17648ed209b2cf8bf823bddce71464
Ed <edsagoefvr@gmail.com>
2022-10-22 20:18:42 +0000

    tested conditions

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Rock Paper Scissors
 
-1. take user input and stoe in a variable
+## Pseudocode
+1. take user input and store in a variable
+2. Make a list of moves. i.e `["rock", "paper", "scissors"]`
+3. Use `random.choice()` on `cpu_move` so that the computer selects moves randomly.
+    - import `random` to be able to use `random.choice()`
+4. print `user_move` and `cpu_move` to update user
+5. Set conditions for winning *(rock smashes scissors, paper covers rock, scissors cuts paper)*
+    - if player = cpu, it's a tie
+    - if player = rock and cpu = scissors, player wins
+        else if cpu = paper, cpu wins
+    - if player = paper and cpu = rock, player wins
+        else if cpu = scissors, cpu wins
+    - if player = scissors and cpu = paper, player wins
+        else if cpu = rock, cpu wins
--- start.py
+++ start.py
@@ -1,10 +1,25 @@
 import random
 
 
-player_move = input("Rock, Paper, Scissors? ")
+player_move = input("rock, paper, scissors? ")
 
 moves = ["rock", "paper", "scissors"]
 
 cpu_move = random.choice(moves)
 
 print(f"You chose {player_move}, cpu chose {cpu_move}.\n")
+
+if player_move == cpu_move:
+    print(f"We both selected {player_move}. Tie!")
+elif player_move == "rock" and cpu_move == "scissors":
+    print("Rock smashes scissors! You win!")
+elif player_move == "rock" and cpu_move == "paper":
+    print("Paper covers rock! SIT DOWN!! I win")
+elif player_move == "paper" and cpu_move == "rock":
+    print("Paper covers rock! You win!")
+elif player_move == "paper" and cpu_move == "scissors":
+    print("Scissors cuts paper! HAHA I win!")
+elif player_move == "scissors" and cpu_move == "paper":
+    print("Scissors cuts paper! You win you lucky bastard!")
+elif player_move == "scissors" and cpu_move == "rock":
+    print('Rock smashes scissors! I win! I "trashes" you!')
